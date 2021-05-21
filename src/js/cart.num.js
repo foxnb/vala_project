ee230809ebed365afb0bffad2f099e814b973729
Nataliya <chnat90@gmail.com>
2021-05-21 21:36:19 +0300

    feat: add src

---
+++ src/js/cart.num.js
@@ -0,0 +1,70 @@
+/****************************************
+ * Plugin for WICart
+ ***************************************/
+var cssfile = document.createElement("link")
+cssfile.setAttribute("rel", "stylesheet")
+cssfile.setAttribute("type", "text/css")
+// cssfile.setAttribute("href", 'wicart.num.css')
+document.getElementsByTagName("head")[0].appendChild(cssfile);
+
+/******************************************
+* глобальный префикс для поля ввода кол-ва
+*****************************************/
+var wiNumInputPrefID;
+
+function WICartNum(cartName, inputClass, prefID) {
+    wiNumInputPrefID = prefID;
+
+    $("input[type=text]." + inputClass).each(function () {
+        var inputID = this.id;
+
+        $('<div onclick="WICartNumSub(\'' + inputID + '\')" class="' + inputClass + '_downbutton">-</div>').insertBefore(this);
+        $('<div onclick="WICartNumAdd(\'' + inputID + '\')" class="' + inputClass + '_addbutton">+</div>').insertAfter(this);
+
+    });
+
+}
+
+function WICartNumSub(id) {
+    var errorMessage = 'Недопустимое значение';
+    var minValue = parseInt($("#" + id).data("min-value"));
+    var val = parseInt($("#" + id).val());
+    val = (val > minValue) ? val - 1 : val;
+    $("#" + id).val(val);
+    WIitemSum(id);
+
+
+}
+
+function WICartNumAdd(id) {
+    var errorMessage = 'Недопустимое значение';
+    var maxValue = parseInt($("#" + id).data("max-value"));
+    var val = parseInt($("#" + id).val());
+    val = (val < maxValue) ? val + 1 : val;
+    $("#" + id).val(val);
+    WIitemSum(id);
+}
+
+function WIitemSum(id) // num
+{
+
+    var ud = id.substr(id.indexOf("_") + 1);
+
+    if ($("#wicartitemsum_" + ud).length) {
+        $("#wicartitemsum_" + ud).html(parseFloat($("#" + id).val() * parseFloat($("#wicartprice_" + ud).html())));
+    }
+}
+
+/**********************************************
+* Иницилизация начальных значений
+**********************************************/
+
+$(document).ready(function () {
+
+    $(".wicartnum").each(function () {
+
+        WIitemSum(this.id)
+
+    });
+
+});
---
+++ src/mailer/mail.php
@@ -0,0 +1,18 @@
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['phone']))
+$phone = $_POST['phone'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+
+
+
+$content="From: $name Phone: $phone  message: $message";
+$recipient = "kippgloria@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email sent!";
+?>
